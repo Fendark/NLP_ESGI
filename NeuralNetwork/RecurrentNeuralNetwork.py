74059e051d7c196d8ea491fbed9f73b9b6b6cb94
bz10bis <baptistezegre@gmail.com>
2018-01-21 11:09:47 +0100

    Correct NN implem

--- NeuralNetwork/RecurrentNeuralNetwork.py
+++ NeuralNetwork/RecurrentNeuralNetwork.py
@@ -1,9 +1,7 @@
 from keras.models import load_model, save_model
-from .neural_network import NeuralNetwork
 from keras.layers import concatenate
-from keras.layers import Input, Embedding, Dropout, Bidirectional, LSTM, Dense, TimeDistributed, Flatten
-from keras.models import Model
-
+from keras.layers import Input, Embedding, Dropout, Bidirectional, LSTM, Dense, TimeDistributed, Flatten, Recurrent
+from keras.models import Model, load_model
 
 class RecurrentNeuralNetwork():
     def __init__(self):
@@ -31,30 +29,6 @@ class RecurrentNeuralNetwork():
         else:
             return (proba > 0.5).astype('int32')
 
-    @classmethod
-    def build_sequence(cls, word_embeddings, input_shape: dict, out_shape: int, units=100, dropout_rate=0.5):
-
-        word_input = Input(shape=(None,), dtype='int32', name='word_input')
-
-        weights = word_embeddings.syn0
-        word_embeddings = Embedding(input_dim=weights.shape[0], output_dim=weights.shape[1],
-                                    weights=[weights], name="word_embeddings_layer", trainable=False,
-                                    mask_zero=True)(word_input)
-
-        pos_input = Input(shape=(None,), dtype='int32', name='pos_input')
-        pos_embeddings = Embedding(input_shape['pos'][0], input_shape['pos'][1], name='pos_embeddings_layer',
-                                   mask_zero=True)(pos_input)
-
-        shape_input = Input(shape=(None,), dtype='int32', name='shape_input')
-        shape_embeddings = Embedding(input_shape['shape'][0], input_shape['shape'][1], name='shape_embeddings_layer',
-                                     mask_zero=True)(shape_input)
-
-        merged_input = concatenate([word_embeddings, pos_embeddings, shape_embeddings], axis=-1)
-
-        # Build the rest of the model here
-
-        print(model.summary())
-        return Recurrent(model)
 
     @classmethod
     def build_classification(cls, word_embeddings, input_shape: dict, out_shape: int, units=100,
@@ -77,7 +51,30 @@ class RecurrentNeuralNetwork():
 
         merged_input = concatenate([word_embeddings, pos_embeddings, shape_embeddings], axis=-1)
 
-        # Build the rest of the model here
+        out_classes = 2
+        print('Build Bi-RNN model...')
+        # Input definition
+        word_input = Input(shape=(100,), dtype='int32', name='word_input')
+
+        # Define layers
+        word_embeddings = Embedding(input_dim=vocab_size, output_dim=50, trainable=True, mask_zero=True)(word_input)
+
+        word_embeddings = Dropout(0.5, name='first_dropout')(word_embeddings)
+
+        bilstm = Bidirectional(LSTM(100, activation='tanh', return_sequences=True), name='bi-lstm')(word_embeddings)
+
+        lstm = LSTM(100, activation='tanh', name='lstm', return_sequences=True)(bilstm)
+
+        lstm = Dropout(0.5, name='second_dropout')(lstm)
+
+        output = TimeDistributed(Dense(out_classes, activation='softmax'))(lstm)
+
+        # Build and compile model
+        model = Model(inputs=[word_input], outputs=output)
+
+        model.compile(optimizer='adam',
+                      loss='binary_crossentropy',
+                      metrics=['accuracy'])
 
         print(model.summary())
         return Recurrent(model)
