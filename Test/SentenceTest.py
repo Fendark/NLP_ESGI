9893ff32c09112a6e72f69eea22c1ef738b4e864
Gerome CABEZAS <gerome.cabezas@gmail.com>
2018-01-21 22:51:45 +0100

    testons, testons.

--- Test/SentenceTest.py
+++ Test/SentenceTest.py
@@ -1,7 +1,14 @@
 import unittest
 from DocumentObject import Sentence
+from DocumentObject import Document
 
-class DocumentTest(unittest.TestCase):
+class SentenceTest(unittest.TestCase):
 
     def test(self):
-        return ""
+        text = "Hello, I am a jolly platypus. " \
+               "I like to run and roll on tremendous hills. " \
+               "I do love to run and roll, but i like to sing too! " \
+               "So the jolly platypus ran, rolled and sung on tremendous hills. "
+        doc = Document.create_from_text(text)
+
+        sentence = Sentence(doc,0,100)
---
+++ Test/TokenTest.py
@@ -0,0 +1,22 @@
+from DocumentObject import Token
+from DocumentObject import Document
+import unittest
+
+class TokenTest(unittest.TestCase):
+
+    def test_sample(self):
+        text = "Hello, I am a jolly platypus. " \
+               "I like to run and roll on tremendous hills. " \
+               "I do love to run and roll, but i like to sing too! " \
+               "So the jolly platypus ran, rolled and sung on tremendous hills. "
+        doc = Document.create_from_text(text)
+
+        token = Token(doc,)
+
+        token.__repr__()
+
+        #self.assertEqual(len(doc.tokens), 46, 'ERROR : tokens')
+        #self.assertEqual(len(doc.sentences), 4, 'ERROR : sentences')
+
+if __name__ == "__main__":
+    unittest.main()
